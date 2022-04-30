fa7dbdd104decc8018297077d3e79180709beb2a
alexanderberg <alexander.berg1@learnet.se>
2022-04-30 20:22:56 +0200

    new migrations after modifying Product.Image

---
+++ hakims-livs/Migrations/20220430142449_fileupload.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace hakims_livs.Migrations
+{
+    public partial class fileupload : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "Products",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "Image",
+                table: "Products",
+                type: "varbinary(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+        }
+    }
+}
