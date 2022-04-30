e59e6edb584eadbeb7ec6a840af70fafc11679e6
alexanderberg <alexander.berg1@learnet.se>
2022-04-30 22:17:24 +0200

    delete image from storage if Product is deleted

--- hakims-livs/Utils/FileStorage.cs
+++ hakims-livs/Utils/FileStorage.cs
@@ -2,11 +2,12 @@ namespace hakims_livs.Utils;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-public static class HostEnvironment{
-    public static IWebHostEnvironment? GetWebHostEnvironment()
+static class HostEnvironment{
+    public static string? GetWebRootPath()
     {
         var _accessor = new HttpContextAccessor();
-        return _accessor.HttpContext?.RequestServices.GetRequiredService<IWebHostEnvironment>();
+        var webHostEnvironment = _accessor.HttpContext?.RequestServices.GetRequiredService<IWebHostEnvironment>();
+        return webHostEnvironment?.WebRootPath;
     }
 }
 public static class FileStorage
@@ -17,11 +18,11 @@ public static class FileStorage
         string fileName;
         try
         {
-            var wwwRootPath = HostEnvironment.GetWebHostEnvironment()?.WebRootPath;
+            var wwwRootPath = HostEnvironment.GetWebRootPath();
             var extension = Path.GetExtension(file.FileName);
             fileName = DateTime.Now.ToString("yymmssfff") + "_" + name + extension;
 
-            var folder = file.ContentType.Contains("image") ? "/images/" : "/uploads/";
+            var folder = GetFolder(extension);
             var path = Path.Combine(wwwRootPath + folder, fileName);
             await using var fileStream = new FileStream(path, FileMode.Create); 
             await file.CopyToAsync(fileStream);
@@ -33,5 +34,25 @@ public static class FileStorage
 
         
         return fileName;
+    }
+
+    private static string GetFolder(string extension)
+    {
+        extension = extension.ToLower();
+        var folder = extension is ".png" or ".jpg" or ".jpeg" or ".gif" or ".tif" ? "/images/" : "/uploads/";
+        return folder;
+    }
+
+    public static void Delete(string fileName)
+    {
+        var extension = Path.GetExtension(fileName);
+        var folder = GetFolder(extension);
+        var wwwRootPath = HostEnvironment.GetWebRootPath();
+        var path = Path.Combine(wwwRootPath + folder + fileName);
+
+        if (File.Exists(path))
+        {
+            File.Delete(path);
+        }
     }
 }
