b4dae6af12508eb46504bb5c2e354b5f41ee9912
Andreas Vernervinge <andreas.vernervinge@gmail.com>
2022-05-02 14:11:31 +0100

    fixed email confirmation and reset pwd

---
+++ hakims-livs/Services/EmailSender.cs
@@ -0,0 +1,37 @@
+﻿using hakims_livs.Models;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net;
+using System.Net.Mail;
+
+namespace hakims_livs.Services
+{
+
+    public class EmailSender : IEmailSender
+    {
+        public EmailSender()
+        {
+
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string body)
+        {
+            var fromMail = "hakimslivs@gmail.com";
+            var fromPassword = "epyzurfddtgfdisv";
+
+            var message = new MailMessage();
+            message.From = new MailAddress(fromMail);
+            message.Subject = subject;
+            message.To.Add(new MailAddress(email));
+            message.Body = $"<html><body> {body} </body> </html>";
+            message.IsBodyHtml = true;
+
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(fromMail, fromPassword),
+                EnableSsl = true
+            };
+            smtpClient.Send(message);
+        }
+    }
+}
