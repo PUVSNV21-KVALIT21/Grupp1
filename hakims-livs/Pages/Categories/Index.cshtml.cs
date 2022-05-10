967cf2f3b92ccfc697d4fa18b55c2061bde7fc04
Amasti0001 <71552480+Amasti0001@users.noreply.github.com>
2022-05-10 13:49:32 +0200

    Added authorization on Cathegory pages

--- hakims-livs/Pages/Categories/Index.cshtml.cs
+++ hakims-livs/Pages/Categories/Index.cshtml.cs
@@ -3,9 +3,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using hakims_livs.Data;
 using hakims_livs.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace hakims_livs.Pages.Categories
 {
+    [Authorize(Roles = "admin")]
     public class IndexModel : PageModel
     {
         private readonly ApplicationDbContext _context;
