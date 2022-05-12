e6caee4ff7398700fecd56fb471392a3d008ac30
alexanderberg <alexander.berg1@learnet.se>
2022-05-12 20:55:55 +0200

    order

---
+++ hakims-livs/Pages/Orders/Details.cshtml.cs
@@ -0,0 +1,41 @@
+#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using hakims_livs.Data;
+using hakims_livs.Models;
+
+namespace hakims_livs.Pages.Orders
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DetailsModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Order Order { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Order = await _context.Orders.Include(o => o.OrderRows).ThenInclude(or => or.Product).FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Order == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
