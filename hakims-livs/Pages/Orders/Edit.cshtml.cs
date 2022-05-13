bde9d7e026e81f6576413b282b7b751c0fb2c58b
Amasti0001 <71552480+Amasti0001@users.noreply.github.com>
2022-05-13 10:14:25 +0200

    Added order view for hakim and customers

---
+++ hakims-livs/Pages/Orders/Edit.cshtml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using hakims_livs.Data;
+using hakims_livs.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace hakims_livs.Pages.Orders
+{
+    [Authorize(Roles = "admin")]
+
+    public class EditModel : PageModel
+    {
+        private readonly hakims_livs.Data.ApplicationDbContext _context;
+
+        public EditModel(hakims_livs.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Order Order { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders.Include(o => o.Customer).Include(o => o.OrderRows).ThenInclude(or => or.Product).FirstOrDefaultAsync(m => m.ID == id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+            Order = order;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Order).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(Order.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool OrderExists(int id)
+        {
+          return (_context.Orders?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
