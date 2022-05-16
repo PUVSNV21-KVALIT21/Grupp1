ef003db9421c4eed910bfb098fa3f504454ef8eb
Johan Strandberg <johan.c.as@outlook.com>
2022-05-16 08:40:19 +0200

    payment options, payment validation

---
+++ hakims-livs/wwwroot/js/components/paymentOption.js
@@ -0,0 +1,21 @@
+﻿const cardPayment = document.getElementById("cardPaymentContainer");
+const swishPayment = document.getElementById("swishPaymentContainer");
+const invoicePayment = document.getElementById("invoicePaymentContainer");
+
+document.getElementById("paymentOption").addEventListener("change", function () {
+    if (this.value == "card") {
+        cardPayment.removeAttribute("hidden")
+        swishPayment.setAttribute("hidden", true)
+        invoicePayment.setAttribute("hidden", true)
+    }
+    else if (this.value == "swish") {
+        swishPayment.removeAttribute("hidden")
+        cardPayment.setAttribute("hidden", true)
+        invoicePayment.setAttribute("hidden", true)
+    }
+    else if (this.value == "invoice") {
+        invoicePayment.removeAttribute("hidden")
+        swishPayment.setAttribute("hidden", true)
+        cardPayment.setAttribute("hidden", true)
+    }
+})
